d4b34154d67644c9aecbf9819d64c82af5497d26
sidneychen <sidneychen@tencent.com>
2020-11-18 16:30:05 +0800

    action add mysql and redis

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module github.com/chenxilinsidney/godemo
 
 go 1.14
+
+require (
+	github.com/go-redis/redis/v8 v8.3.4
+	gorm.io/driver/mysql v1.0.3
+	gorm.io/driver/postgres v1.0.5
+	gorm.io/driver/sqlite v1.1.3
+	gorm.io/driver/sqlserver v1.0.5
+	gorm.io/gorm v1.20.6
+)
--- main_test.go
+++ main_test.go
@@ -1,6 +1,84 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"context"
+
+	"github.com/go-redis/redis/v8"
+	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
+	"gorm.io/driver/sqlite"
+	"gorm.io/driver/sqlserver"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var DB *gorm.DB
+var ctx = context.Background()
+
+func init() {
+	var err error
+	if DB, err = OpenTestConnection(); err != nil {
+		log.Printf("failed to connect database, got error %v", err)
+		os.Exit(1)
+	} else {
+		sqlDB, err := DB.DB()
+		if err == nil {
+			err = sqlDB.Ping()
+		}
+
+		if err != nil {
+			log.Printf("failed to connect database, got error %v", err)
+		}
+
+		if DB.Dialector.Name() == "sqlite" {
+			DB.Exec("PRAGMA foreign_keys = ON")
+		}
+	}
+}
+
+func TestAPI(t *testing.T) {
+	// mysql
+	if value, ok := DB.Set("hello", "world").Get("hello"); !ok {
+		t.Errorf("Should be able to get setting after set")
+	} else {
+		if value.(string) != "world" {
+			t.Errorf("Setted value should not be changed")
+		}
+	}
+
+	if _, ok := DB.Get("non_existing"); ok {
+		t.Errorf("Get non existing key should return error")
+	}
+
+	// redis
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	err := rdb.Set(ctx, "key", "value", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	val, err := rdb.Get(ctx, "key").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("key", val)
+
+	// server
+	go func() {
+		SayHello()
+	}()
+}
 
 func TestSayHello(t *testing.T) {
 	tests := []struct {
@@ -20,4 +98,49 @@ func TestSayHello(t *testing.T) {
 			}
 		})
 	}
+}
+
+func OpenTestConnection() (db *gorm.DB, err error) {
+	dbDSN := os.Getenv("GORM_DSN")
+	switch os.Getenv("GORM_DIALECT") {
+	case "mysql":
+		log.Println("testing mysql...")
+		if dbDSN == "" {
+			dbDSN = "gorm:gorm@tcp(localhost:9910)/gorm?charset=utf8&parseTime=True&loc=Local"
+		}
+		db, err = gorm.Open(mysql.Open(dbDSN), &gorm.Config{})
+	case "postgres":
+		log.Println("testing postgres...")
+		if dbDSN == "" {
+			dbDSN = "user=gorm password=gorm dbname=gorm host=localhost port=9920 sslmode=disable TimeZone=Asia/Shanghai"
+		}
+		db, err = gorm.Open(postgres.New(postgres.Config{
+			DSN:                  dbDSN,
+			PreferSimpleProtocol: true,
+		}), &gorm.Config{})
+	case "sqlserver":
+		// CREATE LOGIN gorm WITH PASSWORD = 'LoremIpsum86';
+		// CREATE DATABASE gorm;
+		// USE gorm;
+		// CREATE USER gorm FROM LOGIN gorm;
+		// sp_changedbowner 'gorm';
+		// npm install -g sql-cli
+		// mssql -u gorm -p LoremIpsum86 -d gorm -o 9930
+		log.Println("testing sqlserver...")
+		if dbDSN == "" {
+			dbDSN = "sqlserver://gorm:LoremIpsum86@localhost:9930?database=gorm"
+		}
+		db, err = gorm.Open(sqlserver.Open(dbDSN), &gorm.Config{})
+	default:
+		log.Println("testing sqlite3...")
+		db, err = gorm.Open(sqlite.Open(filepath.Join(os.TempDir(), "gorm.db")), &gorm.Config{})
+	}
+
+	if debug := os.Getenv("DEBUG"); debug == "true" {
+		db.Logger = db.Logger.LogMode(logger.Info)
+	} else if debug == "false" {
+		db.Logger = db.Logger.LogMode(logger.Silent)
+	}
+
+	return
 }
